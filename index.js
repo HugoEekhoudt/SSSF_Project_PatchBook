843d28fd1f8ecb19080db22b812b644a635d7587
Hugo Eekhoudt <HMP.Eekhoudt@student.han.nl>
2019-04-09 21:35:03 +0300

    beginnning
    made a beginning on login in

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ const app = express();
 const mongoose = require('mongoose');
 var bodyParser = require('body-parser')
 const session = require('express-session')
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 app.use(bodyParser())
 app.use(session({ secret: process.env.SessionSeed, resave: false, saveUninitialized: false }))
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session())
+
+
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+  username:  String,
+  password: String,
+});
+const User = mongoose.model('User', userSchema);
 
 // if mongoose < 5.x, force ES6 Promise
 // mongoose.Promise = global.Promise;
@@ -17,14 +29,41 @@ mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@${proce
   console.log('Connection to db failed: ' + err);
 });
 
-var Schema = mongoose.Schema;
-var userSchema = new Schema({
-  username:  String,
-  password: String,
-});
-const User = mongoose.model('User', userSchema);
+const login = (req, res, next) => {
+    if(req.user){
+        next()
+    }else{
+        res.redirect('/login')
+    }
+  }
+  
+  passport.use(new LocalStrategy(
+    (username, password, done) => {
+        if (username !== process.env.USER || password !== process.env.PASS) {
+            console.log("fout")
+            done(null, false, {message: 'Incorrect credentials.'});
+            return;
+        }
+        console.log("Ingelogd")
+        return done(null, {id: process.env.USER}); // returned object usally contains something to identify the user
+    }
+));
+  
+  passport.serializeUser(function(user, cb) {
+    console.log('Serial')
+    cb(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, cb) {
+    console.log('Deserial')
+    cb(null, {id: id});
+  });
 
 app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+});
+
+app.get('/index', login, (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 });
 
@@ -33,7 +72,9 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-    res.sendFile(__dirname + '/views/loginPage.html')
+    passport.authenticate('local', { 
+        successRedirect: '/index', 
+        failureRedirect: '/login' })
 });
 
 app.post('/register', (req, res) => {
@@ -47,7 +88,8 @@ app.post('/register', (req, res) => {
 });
 
 app.get('/logout', (req, res) => {
-    res.sendFile(__dirname + '/views/loginPage.html')
+    req.logout()
+    res.redirect('/login')
 });
 
 app.listen(3000); 
